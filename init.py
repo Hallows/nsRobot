842570768e9864e8b9c95a14b49a5cc782aca172
XYin <yinxiangbo@me.com>
2021-02-03 19:00:22 +0800

    add init.py

---
+++ init.py
@@ -0,0 +1,10 @@
+dbHost = 'localhost'
+dbPort = 3306
+dbUser = 'nsBot'
+dbPassword = 'nanshanrobot'
+dbName = 'ns_db'
+dbCharset = 'utf8'
+
+MENTAL_ICON_PATH = "./mental_icon/"
+IMAGE_PATH = "./temp/img/"
+FONT_PATH = "./font/"
