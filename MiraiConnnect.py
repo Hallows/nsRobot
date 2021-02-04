4669df642989857b061deea6a87eb02b41c6afc6
Hallows <yinxiangbo666@gmail.com>
2021-02-04 15:56:00 +0800

    封装了部分mirai的链接方法作为MiraiConnect，测试通过 另添加了gitignore忽略不同的VSCode开发环境带来的中间文件

---
+++ MiraiConnnect.py
@@ -0,0 +1,65 @@
+import requests
+import json
+import sys
+
+#获取mirai版本
+#输入-miraiURL: mirai的HTTPAPI地址
+def getVersion(miraiURL):
+    url=miraiURL+'/about'
+    res = requests.get(url=url)
+    jsonData = res.json()
+    if jsonData['data']['version']:
+        print('Connect success, got API version={}'.format(jsonData['data']['version']))
+    else:
+        raise SystemExit('ERROR: Connect failed, programme exit')
+
+#获取授权码
+#输入-miraiURL: mirai的HTTPAPI地址
+#输入-miraiKey: mirai的默认连接key，从配置文件中获取
+#输出-sessionKey: 返回的有效的session
+def getAuth(miraiURL, miraiKey):
+    url=miraiURL+'/auth'
+    requestData = {'authKey': miraiKey}
+    res = requests.post(url=url,json=requestData)
+    jsonData = res.json()
+    if jsonData['code'] == 0:
+        print('Authorize Success! Got AuthCode {}'.format(jsonData['session']))
+        sessionKey = jsonData['session']
+        return sessionKey
+    else:
+        raise SystemExit('ERROR: Authorize Failed with code {} and the authKey is {}'.format(jsonData['code'], requestData['authKey']))
+
+#校验sesson并绑定bot
+#输入-miraiURL: mirai的HTTPAPI地址
+#输入-session: 有效的session，通过getAuth()获取
+#输入-botNumber: 待绑定的机器人的QQ号
+def verify(miraiURL,session,botNumber):
+    requestData = {
+        'sessionKey': session,
+        'qq': botNumber
+        }
+    url=miraiURL+'/verify'
+    res = requests.post(url=url, json=requestData)
+    jsonData = res.json()
+    if jsonData['code'] == 0:
+        print('Verify Success!')
+    else:
+        raise SystemExit('ERROR: Verify Failed!')
+
+
+#释放sesson并清除bot的信息缓存(程序结束前调用否则可能导致溢出)
+#输入-miraiURL: mirai的HTTPAPI地址
+#输入-session: 有效的session，通过getAuth()获取
+#输入-botNumber: 待绑定的机器人的QQ号
+def release(miraiURL,session,botNumber):
+    requestData = {
+        'sessionKey': session,
+        'qq': botNumber
+        }
+    url=miraiURL+'/release'
+    res = requests.post(url=url, json=requestData)
+    jsonData = res.json()
+    if jsonData['code'] == 0:
+        print('Release Success!')
+    else:
+        raise SystemExit('ERROR: Release Failed!')
--- testMirai.py
+++ testMirai.py
@@ -1,42 +1,22 @@
-import requests
-import json
-import sys
+import MiraiConnnect as mirai
 import time
+import requests
+
+miraiURL = 'http://0.0.0.0:8080'
+miraiKey = 'NSROBOTdevmode'
+miraiQQ = 2274927840
 
 #获取版本
-res = requests.get('http://0.0.0.0:8080/about')
-jsonData = res.json()
-if jsonData['data']['version']:
-    print('Connect success, got API version={}'.format(jsonData['data']['version']))
-else:
-    raise SystemExit('ERROR: Connect failed, programme exit')
-
-#鉴权
-requestData = {'authKey': 'NSROBOTdevmode'}
-res = requests.post('http://0.0.0.0:8080/auth',json=requestData)
-jsonData = res.json()
-if jsonData['code'] == 0:
-    print('Authorize Success! Got AuthCode {}'.format(jsonData['session']))
-    sessonKey=jsonData['session']
-else:
-    raise SystemExit('ERROR: Authorize Failed with code {} and the authKey is {}'.format(jsonData['code'], requestData['authKey']))
-
+mirai.getVersion(miraiURL=miraiURL)
+#获取session
+session=mirai.getAuth(miraiURL=miraiURL,miraiKey=miraiKey)
 #校验sesson并绑定bot
-requestData = {
-    'sessionKey': sessonKey,
-    'qq': 2274927840
-    }
-res = requests.post('http://0.0.0.0:8080/verify', json=requestData)
-jsonData = res.json()
-if jsonData['code'] == 0:
-    print('Verify Success!')
-else:
-    raise SystemExit('ERROR: Verify Failed!')
+mirai.verify(miraiURL=miraiURL,session=session,botNumber=miraiQQ)
 
 #发送一条消息
 time.sleep(2)
 requestData = {
-    'sessionKey': sessonKey,
+    'sessionKey': session,
     "target": 773553072,
     "messageChain": [
         { "type": "Plain", "text": "hello\n" },
@@ -50,22 +30,5 @@ if jsonData['code'] == 0:
 else:
     print('Send failed')
 
-#等待回复
-time.sleep(10)
-
-#获取接收
-res = requests.get('http://0.0.0.0:8080/fetchLatestMessage', params= {'sessionKey': sessonKey, 'count': 1})
-print(jsonData['data'][0]['messageChain'][1]['text'])
-
-
 #释放sesson
-requestData = {
-    'sessionKey': sessonKey,
-    'qq': 2274927840
-    }
-res = requests.post('http://0.0.0.0:8080/release', json=requestData)
-jsonData = res.json()
-if jsonData['code'] == 0:
-    print('Release Success!')
-else:
-    raise SystemExit('ERROR: Release Failed!')
+mirai.release(miraiURL=miraiURL,session=session,botNumber=miraiQQ)
