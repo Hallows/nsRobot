137179e40ec48fb5aee00da459b9998d170069a2
辰韶 <841546312@qq.com>
2021-02-05 22:26:39 +0800

    删除了‘辰韶’的input

--- input.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-import init
-
-"命令格式：ns报团 团id 心法 "
-
-
-def Input(data: list):
-    db = pymysql.connect(host=init.dbHost, port=init.dbPort, user=init.dbUser,
-                         password=init.dbPassword, db=init.dbName, charset=init.dbCharset)
