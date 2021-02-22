276c5dd1a20909149e1e0832676002e59e0306e3
Magicat128 <48112124+Magicat128@users.noreply.github.com>
2021-02-22 16:47:30 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,24 +3,21 @@ from pyunit_time import Time
 
 
 def parseDate(date):
-  res = re.findall('^\d{1,2}月\d{1,2}', date)
-  if res:
-    mm, dd = res[0].split('月')
-    date = mm + '月' + dd + '日'
+    tokens = ['月', '-', '.', '/', '\']
+  
+    for token in tokens:
+        regex = re.findall('^\d{1,2}\'+token+'\d{1,2}', date)
+        if regex:
+            mm, dd = regex[0].split(token)
+            date = mm + '月' + dd + '日'
 
-  res = re.findall('^\d{1,2}-\d{1,2}', date)
-  if res:
-    mm, dd = res[0].split('-')
-    date = mm + '月' + dd + '日'
+    try:
+        res = Time().parse(date)
+        res = res[0]['keyDate'][:10]
+        #for i in range(len(res)):
+        #    print(res[i]['keyDate'][5:10] + ' ')
+    except Exception as ex:
+        print(str(ex))
+        res = '日期错误！'
 
-  res = re.findall('^\d{1,2}\.\d{1,2}', date)
-  if res:
-    mm, dd = res[0].split('.')
-    date = mm + '月' + dd + '日'
-
-  try:
-    res = Time().parse(date)
-    for i in range(len(res)):
-      print(res[i]['keyDate'][5:10] + ' ')
-  except:
-    print('日期不存在！')
+    return res
