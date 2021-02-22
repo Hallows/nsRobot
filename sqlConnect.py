bb2139c9b2a62d0b530c0800b65fdd747e9278e8
Hallows <yinxiangbo666@gmail.com>
2021-02-22 16:20:46 +0800

    修复了多个问题，现在新建团队可用了

--- sqlConnect.py
+++ sqlConnect.py
@@ -9,11 +9,11 @@ import pymysql
 #如果团长正常存在，返回该团长在数据库内的ID用于后续操作
 def has_Leader(db, leaderQQ):
     cursor = db.cursor()
-    command="SELECT * FROM ns_leader WHERE QQNumber = '{}' AND effective = 0".format(QQ)
+    command="SELECT * FROM ns_leader WHERE QQNumber = '{}' AND effective = 0".format(leaderQQ)
     cursor.execute(command)
     if cursor.rowcount != 0:
         result = cursor.fetchone()
-        return result[id]
+        return result[0]
     else:
         return - 1  #权限错误
 
@@ -21,7 +21,7 @@ def has_Leader(db, leaderQQ):
 #-------输入---------
 #db:有效的数据库连接
 #date:开团日期（格式示例:'2021-02-09'）
-#Time:开团时间（格式示例:'21:05:00'）
+#Time:开团时间（格式示例:'21:05'）
 #dungeon:副本名称，字符串
 #comment:要求与说明，字符串
 #useBlackList：是否启用黑名单，1为启用0为不启用，默认为0
@@ -29,15 +29,19 @@ def has_Leader(db, leaderQQ):
 #-------输出---------
 #如果开团成功，返回新开团队的团队ID
 #如果开团失败，返回-1
-def createNewTeam(db, date, time, dungeon, comment:0, useBlackList,leaderID):
+def createNewTeam(db, date, time, dungeon, comment, useBlackList=0,leaderID):
     cursor = db.cursor()
-    inputTime="{} {}".format(date,time)
-    command="INSERT INTO ns_team(leaderID,dungeon,startTime,effective,allowBlackList,remark) VALUES({},{},{},0,{},{})".format(leaderID,dungeon,inputTime,useBlackList,remark)
-    cursor.execute(command)
-    command = "SELECT * FROM  ns_team WHERE startTime={}".format(inputTime)
-    cursor.execute(command)
+    try:
+        command = "INSERT INTO ns_team(leaderID,dungeon,startDate,startTime,effective,allowBlackList,remark) VALUES({},'{}','{}','{}',0,{},'{}')".format(leaderID, dungeon, date, time, useBlackList, comment)
+        cursor.execute(command)
+        db.commit()
+        command = "SELECT * FROM ns_team WHERE startDate='{}' AND leaderID={} AND startTime='{}'".format(date, leaderID, time)
+        cursor.execute(command)
+    except:
+        db.rollback()
+        return -1 #开团失败
     if cursor.rowcount != 0:
         result = cursor.fetchone()
-        return result[teamID]
+        return result[0]
     else:
         return -1 #开团失败
